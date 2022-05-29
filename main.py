3e98328ba06fb4799846aab07d6a557c55b60562
hussenk <hussen199710@yahoo.com>
2022-05-29 22:23:17 +0200

    [A] read rows

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, url_for, redirect
 from serves import serves
 app = Flask(__name__)
 
-srv = serves()
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -12,15 +11,16 @@ def index():
 
 @app.route('/file', methods=['GET','POST'])
 def read():
+    srv = serves()
+
     srv.handelErrorUpload()
+
+    srv.loadFile()
 
     if request.form.get('dColumnsCB') == 'on':
         srv.deleteColumn()
 
-    if request.form.get('newHeadersCB') == 'on':
-        srv.setNewHeaders()
-    
-
+    srv.saveCsv()
 
     return 'file'
 
--- serves.py
+++ serves.py
@@ -1,21 +1,20 @@
-from turtle import st
-from unicodedata import name
-from flask import Flask, render_template, request, url_for, redirect
+import csv
+from flask import request, redirect
+from openpyxl import load_workbook
+
 
 class serves:
 
     def __init__(self):
-        pass
-    
-    def stringSpelter(self,str):
-      return str.replace(';',' ').replace(',',' ').replace('.',' ').replace('،',' ').replace('-',' ').split()
+        self.arrayData = []
 
-
+    def stringSpelter(self, str):
+        return str.replace(';', ' ').replace(',', ' ').replace('.', ' ').replace('،', ' ').replace('-', ' ').split()
 
     def handelErrorUpload(self):
         # check method
         # print('check  request method')
-        if(request.method== 'GET'):
+        if(request.method == 'GET'):
             return redirect('/')
         # check if file exist
         # print('check if file exist')
@@ -25,28 +24,67 @@ class serves:
         self.dColumns = self.stringSpelter(request.form['dColumns'])
         self.newHeaders = self.stringSpelter(request.form['newHeaders'])
         # print(self.dColumns)
-        print(self.newHeaders)
+        # print(self.newHeaders)
         self.file = request.files['upload']
-        
-        # getting file name and extension
-        self.name,  self.extension =  self.file.filename.rsplit('.',1)
-        print('getting file name and extension')
 
-        # check type file 
-        # print('check type file')   
+        # getting file name and extension
+        self.name,  self.extension = self.file.filename.rsplit('.', 1)
+        # print('getting file name and extension')
+
+        # check type file
+        # print('check type file')
         if(self.extension != 'xlsx'):
             return redirect('/')
-
-    def handelFile():
         pass
 
+    def readRows(self):
+        self.arrayData = []
+        rows = self.ws.iter_rows(2)
+        # print(rows)
+        for row in rows:
+            data = {}
+            for title, cell in zip(self.headers, row):
+                data[title] = cell.value
+            self.arrayData.append(data)
+        # print(self.arrayData)
+
+    def getHeaders(self):
+        self.oldHeaders = [cell.value for cell in next(self.ws.rows)]
+        return len(self.oldHeaders)
+
+    def loadFile(self):
+        wb = load_workbook(self.file)
+        # open file
+        self.ws = wb.active
+        # get old headers of file
+        self.setHeaders()
+        self.readRows()
 
     def deleteColumn(self):
+        # print (self.dColumns)
         for item in self.dColumns:
-            index = headers.index(item)
-            ws.delete_cols(index+1)
-            headers.remove(item)
+            # print(item)
+            index = self.oldHeaders.index(item)
+            # print(index)
+            self.ws.delete_cols(index+1)
+            # print(self.ws)
+            self.oldHeaders.remove(item)
+        self.readRows()
 
-    def getOldHeaders():
-        pass
-    
+    def setHeaders(self):
+        count = self.getHeaders()
+        print(count)
+        if (request.form.get('newHeadersCB') == 'on' and count == len(self.newHeaders)):
+            # print('new headres')
+            self.headers = self.newHeaders
+        else:
+            self.headers = self.oldHeaders
+        self.readRows()
+        return True
+
+    def saveCsv(self):
+        print(self.arrayData)
+        with open('out.csv', 'w', newline='', encoding="utf-8") as output_file:
+            dict_writer = csv.DictWriter(output_file,  self.headers)
+            dict_writer.writeheader()
+            # dict_writer.writerows(self.arrayData)
